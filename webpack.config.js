624e50ad46258d337713a5c078babc431d3bb025
Jesse Hang <hangtimee@gmail.com>
2019-06-05 22:57:50 -0700

    imported sass to js

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,13 @@ module.exports = {
         use: [
           'style-loader', 'css-loader'
         ]
-      }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader', 'css-loader', 'sass-loader'  
+        ]
+      } 
     ]
   }
 }
